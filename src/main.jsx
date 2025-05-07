2feb493dd1bddf4c5b574a33714bf199c5189c53
sandeep Kumar <sandeepkumarthakur9888@gmail.com>
2025-05-07 09:06:10 +0530

    Setup Routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Error from "./pages/Error";
+import SingleMovie from "./pages/SingleMovie";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="movie/:id" element={<SingleMovie />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
+  </BrowserRouter>
+);
--- src/pages/SingleMovie.jsx
+++ src/pages/SingleMovie.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const SingleMovie = () => {
+  const {id} = useParams();
   return (
-    <div>SingleMovie</div>
-  )
-}
+    <>
+      <div>This is Single Movie Page. {id}</div>
+    </>
+  );
+};
 
-export default SingleMovie
+export default SingleMovie;
